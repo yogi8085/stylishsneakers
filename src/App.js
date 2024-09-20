07986bcd53defde17968f96663b6256bc293c447
Yogi8085 <patidaryogi0@gmail.com>
2024-09-20 16:21:29 +0530

    banti

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Main from './components/Main';
+import Preview from './components/Preview';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className='container.fluid'>
+      <div className='row'>
+        <div className='col-8'>
+            <Main />
+          </div>
+          <div className='col-4'>
+            <Preview />
+        </div>
+      </div>
+     </div>
+  </div>
+  </>
   );
 }
 
