e8bd714f6e3c398c394c011510baa32d2839a395
Yogi8085 <patidaryogi0@gmail.com>
2024-09-21 12:28:13 +0530

    all done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './Header'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className='comtainer'>
+    <Header />
+    </div>
   )
 }
 
